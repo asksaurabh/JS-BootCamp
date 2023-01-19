946f6625240026736ca77a95db9d7bba09f84f3d
Saurabh Kumar <asksaurabh101@gmail.com>
2023-01-19 16:11:43 +0530

    Conditional statements

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("Hello World!", true, 3 + 4);
+
+// if statement
+let isUserLoggedIn = true;
+
+if (isUserLoggedIn === true) {
+  console.log("Hey user!, you are logged in.");
+}
+
+// else if and else
+let userRating = 5;
+
+if (userRating < 3) {
+  console.log("How can we improve");
+} else if (userRating >= 3 && userRating <= 4) {
+  console.log("Thank you for rating us.");
+} else {
+  console.log("You like us so much.");
+}
