f301c4257f4f9e8ddde1eb6d9b1f55dc8d036384
Saurabh Kumar <asksaurabh101@gmail.com>
2023-01-26 19:27:29 +0530

    Color picker and the event object

--- app10.js
+++ app10.js
@@ -8,14 +8,43 @@ const btn = document.querySelector("#clicker");
 // };
 
 // Using addEventListener Method.
-btn.addEventListener("click", function () {
-  console.log("You clicked the clicker.");
-});
+// Add multiple event listeners to same element.
+// btn.addEventListener("click", function () {
+//   console.log("You clicked the clicker.");
+// });
 
-btn.addEventListener("mouseover", function () {
-  btn.innerText = "You are hovering over me.";
-});
+// btn.addEventListener("mouseover", function () {
+//   btn.innerText = "You are hovering over me.";
+// });
 
-btn.addEventListener("mouseout", function () {
-  btn.innerText = "Clicker";
-});
+// btn.addEventListener("mouseout", function () {
+//   btn.innerText = "Clicker";
+// });
+
+// Add event to multiple elements.
+const colors = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "purple",
+  "indigo",
+  "violet",
+];
+
+const container = document.querySelector("#boxes");
+const h1 = document.querySelector("h1");
+
+const changeColor = function (event) {
+  console.log(event);
+  h1.style.color = this.style.backgroundColor;
+};
+
+for (let color of colors) {
+  const box = document.createElement("div");
+  box.style.backgroundColor = color;
+  box.classList.add("box");
+  container.appendChild(box);
+  box.addEventListener("click", changeColor);
+}
