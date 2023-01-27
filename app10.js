f9f084400aa4f33a2bf2e268d4470f7199ceca8f
Saurabh Kumar <asksaurabh101@gmail.com>
2023-01-27 11:30:18 +0530

    Keypress events and form events

--- app10.js
+++ app10.js
@@ -22,29 +22,111 @@ const btn = document.querySelector("#clicker");
 // });
 
 // Add event to multiple elements.
-const colors = [
-  "red",
-  "orange",
-  "yellow",
-  "green",
-  "blue",
-  "purple",
-  "indigo",
-  "violet",
-];
+// const colors = [
+//   "red",
+//   "orange",
+//   "yellow",
+//   "green",
+//   "blue",
+//   "purple",
+//   "indigo",
+//   "violet",
+// ];
 
-const container = document.querySelector("#boxes");
-const h1 = document.querySelector("h1");
+// const container = document.querySelector("#boxes");
+// const h1 = document.querySelector("h1");
 
-const changeColor = function (event) {
-  console.log(event);
-  h1.style.color = this.style.backgroundColor;
-};
+// const changeColor = function (event) {
+//   console.log(event);
+//   h1.style.color = this.style.backgroundColor;
+// };
 
-for (let color of colors) {
-  const box = document.createElement("div");
-  box.style.backgroundColor = color;
-  box.classList.add("box");
-  container.appendChild(box);
-  box.addEventListener("click", changeColor);
+// for (let color of colors) {
+//   const box = document.createElement("div");
+//   box.style.backgroundColor = color;
+//   box.classList.add("box");
+//   container.appendChild(box);
+//   box.addEventListener("click", changeColor);
+// }
+
+// Key event: keypress, keyup and keydown
+
+// const input = document.querySelector("#username");
+
+// input.addEventListener("keydown", function (event) {
+//   console.log("Key down");
+// });
+
+// input.addEventListener("keyup", function (event) {
+//   console.log("Key up");
+// });
+
+// input.addEventListener("keypress", function (event) {
+//   console.log("Key Press");
+// });
+
+// Shopping List
+// const addItemInput = document.querySelector("#addItem");
+// const ul = document.querySelector("#items");
+
+// const addItemToItemsList = function () {
+//   const listItem = document.createElement("li");
+//   listItem.innerText = this.value;
+//   ul.appendChild(listItem);
+//   this.value = "";
+// };
+
+// addItemInput.addEventListener("keydown", function (event) {
+//   // console.log(event.key);
+//   if (event.key === "Enter" && this.value) {
+//     addItemToItemsList.call(this);
+//   }
+// });
+
+// Form events
+const form = document.querySelector("#signup-form");
+const creditCard = document.querySelector("#cc");
+const termsCheckbox = document.querySelector("#terms");
+const veggieSelect = document.querySelector("#veggie");
+
+// submit event and preventDefault
+// form.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   console.log("cc", creditCard.value);
+//   console.log("terms", termsCheckbox.checked);
+//   console.log("veggieSelect", veggieSelect.value);
+// });
+
+// input and change events
+const formData = {};
+
+// Method - 1
+// Bad thing about this method is we are hardcoding data into the form.
+
+// creditCard.addEventListener("input", function (event) {
+//   formData["cc"] = event.target.value;
+// });
+
+// veggieSelect.addEventListener("input", function (event) {
+//   formData["veggie"] = event.target.value;
+// });
+
+// termsCheckbox.addEventListener("input", function (event) {
+//   formData["agreeToTerms"] = event.target.checked;
+// });
+
+// Method - 2
+// Add a name attribute to each input..
+
+const inputs = [creditCard, termsCheckbox, veggieSelect];
+
+// Since we need only target from event. Destructure it.
+// Destructure target to what values you want.
+
+for (let input of inputs) {
+  input.addEventListener("input", ({ target }) => {
+    const { name, type, value, checked } = target;
+    formData[name] = type === "checkbox" ? checked : value;
+    console.log(formData);
+  });
 }
