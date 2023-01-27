6ce0939b1a58f92c1b308d9d81a3cbbead4a71ea
Saurabh Kumar <asksaurabh101@gmail.com>
2023-01-27 21:17:19 +0530

    Promise chaining

--- 12_promises_intro.js
+++ 12_promises_intro.js
@@ -42,57 +42,116 @@
 // Resolving / Rejecting with values
 // Try to pretend an http request.
 
+// const fakeRequest = (url) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const pages = {
+//         "/users": [
+//           { id: 1, userName: "Saurabh" },
+//           { id: 2, userName: "Rishabh" },
+//         ],
+//         "/about": "This is the about page",
+//       };
+//       const data = pages[url];
+
+//       if (data) {
+//         resolve({ status: 200, data });
+//       } else {
+//         reject({ status: 404 });
+//       }
+//     }, 1000);
+//   });
+// };
+
+// // Get all the users.
+// fakeRequest("/users")
+//   .then((res) => {
+//     const users = res.data;
+//     console.log(res.status);
+//     console.log(users);
+//   })
+//   .catch((res) => {
+//     console.log(res.status);
+//     console.log("Request rejected to get users.");
+//   });
+
+// // Get the about page.
+// fakeRequest("/about")
+//   .then((res) => {
+//     console.log(res.status);
+//     console.log(res.data);
+//   })
+//   .catch((res) => {
+//     console.log(res.status);
+//     console.log("Request for about page rejected.");
+//   });
+
+// // Get the "/contact page"
+// fakeRequest("/contact")
+//   .then((res) => {
+//     console.log(res.status);
+//     console.log(res.data);
+//   })
+//   .catch((res) => {
+//     console.log(res.status);
+//     console.log("Request for contact page rejected.");
+//   });
+
+// Promise chaining
+//This is a fake http request function. It takes 1 second to resolve or reject the promise, depending on the url that is passed in
+
 const fakeRequest = (url) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const pages = {
         "/users": [
-          { id: 1, userName: "Saurabh" },
-          { id: 2, userName: "Rishabh" },
+          { id: 1, username: "Bilbo" },
+          { id: 5, username: "Esmerelda" },
         ],
-        "/about": "This is the about page",
+        "/users/1": {
+          id: 1,
+          username: "Bilbo",
+          upvotes: 360,
+          city: "Lisbon",
+          topPostId: 454321,
+        },
+        "/users/5": {
+          id: 5,
+          username: "Esmerelda",
+          upvotes: 571,
+          city: "Honolulu",
+        },
+        "/posts/454321": {
+          id: 454321,
+          title: "Ladies & Gentlemen, may I introduce my pet pig, Hamlet",
+        },
+        "/about": "This is the about page!",
       };
       const data = pages[url];
-
       if (data) {
-        resolve({ status: 200, data });
+        resolve({ status: 200, data }); //resolve with a value!
       } else {
-        reject({ status: 404 });
+        reject({ status: 404 }); //reject with a value!
       }
     }, 1000);
   });
 };
 
-// Get all the users.
+// Request for /users and when you get /users, go to /users/1 and when you get /users/1 request for the post of /users/1.
+// Idea is to return a promise on such type of nested calls.
+
 fakeRequest("/users")
   .then((res) => {
-    const users = res.data;
-    console.log(res.status);
-    console.log(users);
+    const userId = res.data[0].id;
+    return fakeRequest(`/users/${userId}`);
   })
-  .catch((res) => {
-    console.log(res.status);
-    console.log("Request rejected to get users.");
-  });
-
-// Get the about page.
-fakeRequest("/about")
   .then((res) => {
-    console.log(res.status);
-    console.log(res.data);
+    const postId = res.data.topPostId;
+    return fakeRequest(`/posts/${postId}`);
   })
-  .catch((res) => {
-    console.log(res.status);
-    console.log("Request for about page rejected.");
-  });
-
-// Get the "/contact page"
-fakeRequest("/contact")
   .then((res) => {
-    console.log(res.status);
-    console.log(res.data);
+    console.log(res);
   })
-  .catch((res) => {
-    console.log(res.status);
-    console.log("Request for contact page rejected.");
+  .catch((err) => {
+    console.log("NO Error..", err);
   });
