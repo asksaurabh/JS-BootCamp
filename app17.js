43200cb24d5506797f80c94f8a3684a2f61a86ed
Saurabh Kumar <asksaurabh101@gmail.com>
2023-01-28 21:57:42 +0530

    Converting functions with promises to async-await functions

--- app17.js
+++ app17.js
@@ -12,52 +12,73 @@
 // Helps create synchronous looking asynchronous code.
 
 // Sum function using convetional syntax of promise
-function getSum(x, y) {
-  return new Promise((resolve, reject) => {
-    if (typeof x !== "number" || typeof y !== "number") {
-      reject("X and Y must be numbers");
-    } else {
-      resolve(x + y);
-    }
-  });
-}
+// function getSum(x, y) {
+//   return new Promise((resolve, reject) => {
+//     if (typeof x !== "number" || typeof y !== "number") {
+//       reject("X and Y must be numbers");
+//     } else {
+//       resolve(x + y);
+//     }
+//   });
+// }
 
-getSum(4, 5)
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// getSum(4, 5)
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-getSum("Hi", 5)
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// getSum("Hi", 5)
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 // Calculate sum using Async functions.
-async function add(x, y) {
-  if (typeof x !== "number" || typeof y !== "number") {
-    throw "Oyi. X and Y must be numbers";
-  }
-  return x + y;
+// Given we know that a async function does return a promise.
+// async function add(x, y) {
+//   if (typeof x !== "number" || typeof y !== "number") {
+//     throw "Oyi. X and Y must be numbers";
+//   }
+//   return x + y;
+// }
+
+// add("Hi", 5)
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// add(6, 5)
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// Part - 3
+// The Await Keyword
+
+// Without async and await.
+// function getPlanets() {
+//   return axios.get("https://swapi.dev/api/planets/");
+// }
+
+// getPlanets().then((res) => {
+//   console.log(res);
+// });
+
+// With async and await.
+async function getPlanets() {
+  const res = await axios.get("https://swapi.dev/api/planets/");
+  console.log(res);
 }
 
-add("Hi", 5)
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-add(6, 5)
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+getPlanets();
