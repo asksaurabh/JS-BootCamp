c98d4d9f1875aead2e272d4ff66748c22b8b3861
Saurabh Kumar <asksaurabh101@gmail.com>
2023-01-28 18:47:05 +0530

    Chaining fetch requests

--- app15.js
+++ app15.js
@@ -1,13 +1,40 @@
 // Get me planets.
 
+// const url = "https://swapi.dev/api/planets";
+// fetch(url)
+//   .then((response) => {
+//     if (!response.ok) throw new Error(`Status Code Error: ${response.status}`);
+
+//     response.json().then((data) => {
+//       console.log(data.results);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("Error..");
+//   });
+
+// First films that planets occur in.
+
 const url = "https://swapi.dev/api/planets";
 fetch(url)
   .then((response) => {
     if (!response.ok) throw new Error(`Status Code Error: ${response.status}`);
 
-    response.json().then((data) => {
-      console.log(data.results);
-    });
+    return response.json();
+  })
+  .then((data) => {
+    console.log("Fetched all Planets(10 on first page");
+    const firstFilmUrl = data.results[0].films[0];
+    return fetch(firstFilmUrl);
+  })
+  .then((response) => {
+    if (!response.ok) throw new Error(`Status Code Error: ${response.status}`);
+
+    return response.json();
+  })
+  .then((data) => {
+    console.log("Fetched the movie planet is looking for");
+    console.log(data.title);
   })
   .catch((err) => {
     console.log("Error..");
